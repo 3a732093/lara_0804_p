7237f7e7fb55408a39a6a07101d13aedb5a25d66
liao wei chen <rax02270521@gmail.com>
2020-10-13 22:42:45 +0800

    autoload && DBsettung

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_PORT', 3306);
-define('DB_DATABASE', '');
+define('DB_PORT', 33061);
+define('DB_DATABASE', 'lara_0804_p');
 define('DB_CHARSET', 'utf8');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
